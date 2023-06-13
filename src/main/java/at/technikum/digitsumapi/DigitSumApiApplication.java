119f191062bf24fc757f2ffb4e426b544377136c
Stella Stepic <wi22b117@technikum-wien.at>
2023-06-13 10:11:26 +0200

    Initial commit

---
+++ src/main/java/at/technikum/digitsumapi/DigitSumApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.digitsumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitSumApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitSumApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum/digitsumapi/DigitSumApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.digitsumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitSumApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
